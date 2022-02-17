3f47bfb76d5ac0580c3f92438a25a69f1a965a1b
Muhamamd Jawad <m.jawad8991@gmail.com>
2022-02-17 15:57:12 +0500

    expections types

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,18 +1,44 @@
+import 'package:flutter/material.dart';
+
 import '../model/picsum_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'app_exceptions.dart';
+
 class Services {
   Future<List<PicSumModel>> fetchPicturesAPI() async {
     String url = 'https://picsum.photos/v2/list';
-    final resposne = await http.get(Uri.parse(url));
+    final response = await http.get(Uri.parse(url));
 
-    if (resposne.statusCode == 200) {
-      final json = jsonDecode(resposne.body) as List;
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as List;
       final listResult = json.map((e) => PicSumModel.fromJson(e)).toList();
       return listResult;
     } else {
       throw Exception('Error fetching Picture');
+    }
+    //with exceptions
+    // return returnResponse(response);
+  }
+
+  @visibleForTesting
+  dynamic returnResponse(response) {
+    switch (response.statusCode) {
+      case 200:
+        final json = jsonDecode(response.body) as List;
+        final listResult = json.map((e) => PicSumModel.fromJson(e)).toList();
+        return listResult;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while communication with server' +
+                ' with status code : ${response.statusCode}');
     }
   }
 }
